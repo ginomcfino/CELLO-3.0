6c780614a1a629301eb18c2d387429f31da9356c
ginomcfino <jiweiqi717@gmail.com>
2023-03-19 06:47:27 -0400

    setting up backend data storage - server & cache

--- UCFormatter/UCFormatter.py
+++ UCFormatter/UCFormatter.py
@@ -1,8 +1,13 @@
 from dash import Dash, dcc, html, Input, Output
+from dash.exceptions import PreventUpdate
 import json
 import os
 import glob
 
+
+# TODO: Implement AWS S3 to host UCF Files
+# TODO: Retrieve UCF files with REST API
+# TODO: Implement AWS ElastiCache for in-memory storage
 ucf_path = '../../IO/inputs'
 cur_dir = os.getcwd()
 os.chdir(ucf_path)
@@ -10,12 +15,15 @@ extension = '.json'
 ucf_files = sorted(list(glob.glob('*' + extension)))
 os.chdir(cur_dir)
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     children=[
+        # Data Storage in Local Meomory
+        # dcc.Store(id='ucf-data', storage_type='local'),
+
+        # html components
         html.H1(
             children='CELLO-V3',
             style={
@@ -62,36 +70,8 @@ app.layout = html.Div(
                     dcc.Dropdown(ucf_files, ucf_files[0], id='ucf_select'),
 
                     html.Br(),
-                    # html.Label('Multi-Select Dropdown'),
-                    # dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
-                    #              ['Montréal', 'San Francisco'],
-                    #              multi=True),
-
-                    # html.Br(),
-                    # html.Label('Radio Items'),
-                    # dcc.RadioItems(['New York City', 'Montréal',
-                    #                'San Francisco'], 'Montréal'),
                 ], style={'padding': 10, 'flex': 1}),
 
-                # html.Div(children=[
-                #     html.Label('Checkboxes'),
-                #     dcc.Checklist(['New York City', 'Montréal', 'San Francisco'],
-                #                 ['Montréal', 'San Francisco']
-                #     ),
-
-                #     html.Br(),
-                #     html.Label('Text Input'),
-                #     dcc.Input(value='MTL', type='text'),
-
-                #     html.Br(),
-                #     html.Label('Slider'),
-                #     dcc.Slider(
-                #         min=0,
-                #         max=9,
-                #         marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 6)},
-                #         value=5,
-                #     ),
-                # ], style={'padding': 10, 'flex': 1})
             ],
             style={
                 'textAlign': 'center',
@@ -102,7 +82,7 @@ app.layout = html.Div(
 
         html.Div([
             html.H5('UCF preview: '),
-            html.Div(id='ucf_name', style={
+            html.Div(id='ucf_preview', style={
                 'padding-left': '100px',
                 'padding-right': '100px'
             }),
@@ -114,24 +94,13 @@ app.layout = html.Div(
                             n_clicks=0, children='Submit')
             ],
             ),
-        ],
+            html.Br(),
+            # html.P(id='ucf_collection_names')
+        ], 
             style={
                 'text-align': 'center',
         }
         ),
-
-        # html.Div(
-        #     children=[
-        #         html.Div(style={'flex': 0.2, 'textAlign': 'center'}),
-        #         html.Div(id='ucf_name', style={'flex': 0.6, 'textAlign': 'center'}),
-        #         html.Div(style={'flex': 0.2, 'textAlign': 'center'}),
-        #     ],
-        #     style={
-        #         'display': 'flex',
-        #         'flex-direction': 'row',
-        #     }
-        # ),
-
 
 
     ],
@@ -141,28 +110,73 @@ app.layout = html.Div(
     }
 )
 
+# @app.callback(
+#     Output('ucf-data', 'data'),
+#     Input('ucf_select', 'value')
+# )
+# def select_ucf(ucf_name):
+#     with open(os.path.join(ucf_path, ucf_name), 'r') as f:
+#         ucf_data = json.load(f)
+#         print('\'Click\'')
+#         print(json.dumps(ucf_data[0], indent=4))
+#         print()
+#         print()
+#     return ucf_data
 
 @app.callback(
-    Output('ucf_name', 'children'),
+    Output('ucf_preview', 'children'),
     Input('ucf_select', 'value')
 )
-def this_function_name_does_not_even_matter(ucf_name):
+def select_ucf(ucf_name):
     with open(os.path.join(ucf_path, ucf_name), 'r') as f:
-        ucf_json = json.load(f)
+        ucf_data = json.load(f)
         print('\'Click\'')
-        print(json.dumps(ucf_json[0], indent=4))
+        print(json.dumps(ucf_data[0], indent=4))
         print()
         print()
     return html.Div(
-        html.Pre(json.dumps(ucf_json[:10], indent=2)),
+        html.Pre(json.dumps(ucf_data[:10], indent=4)),
         style={
             'height': '500px',
             'overflow': 'auto',
-            'white-space': 'pre-wrap',
-            'background-color': 'rgba(128, 128, 128, 0.1)'
+            'white-space': 'nowrap',
+            'background-color': 'rgba(128, 128, 128, 0.1)',
+            'text-align': 'left'
         }
     )
 
+
+# @app.callback(
+#     Output('ucf_preview', 'children'),
+#     Input('ucf-data', 'data')
+# )
+# def this_function_name_does_not_even_matter(ucf):
+#     if ucf is None:
+#         raise PreventUpdate
+#     return html.Div(
+#         html.Pre(json.dumps(ucf[:10], indent=4)),
+#         style={
+#             'height': '500px',
+#             'overflow': 'auto',
+#             'white-space': 'nowrap',
+#             'background-color': 'rgba(128, 128, 128, 0.1)',
+#             'text-align': 'left'
+#         }
+#     )
+    
+# @app.callback(
+#     Output('ucf_collection_names', 'children'),
+#     Input('ucf_preview', 'children')
+# )
+# def get_ucf_collection_names(ucf):
+#     if ucf is None:
+#         print("ERROR")
+#         raise PreventUpdate
+#     collections = []
+#     for i in ucf:
+#         collections.append(i['collection'])
+#     return collections + ' what is this? '
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
