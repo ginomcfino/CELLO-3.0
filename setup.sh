207a62436ca76086495a360c3e365aa4ea16ccbc
ginomcfino <jiweiqi717@gmail.com>
2023-03-21 01:25:36 -0400

    modified setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # source env/bin/activate
 
 # check if brew is installed, and install if necessary
-if ! command -v brew &> /dev/null
+if ! command -v brew & > /dev/null
 then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
